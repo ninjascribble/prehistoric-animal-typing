30f5620c010a91eefa5b634b9647f2d3cbd0605e
ninjascribble <uk.subhuman@gmail.com>
2025-06-12 18:05:29 -0700

    add the input

--- src/main.js
+++ src/main.js
@@ -5,13 +5,10 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <h1>PROTEROSUCHUS</h1>
+    <div class="card">
+      <input id="answer" type="text" />
+    </div>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
